b3f73a301e45540a1bc423f94644fd93bc403e46
abdulazeeza <abdulazeez@brandmobile.net>
2022-05-16 18:02:42 +0100

    complete resume

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,19 @@ import "./Home.scss";
 import { secondaryComponents } from "../../components/secondaryComponents";
 
 const Home = () => {
-    return (
-        <div className="landing-page">
-            <a href="" className="download-resume">Download Resume</a>
-            <secondaryComponents.IntroSection />
-            <secondaryComponents.MainContent />
-        </div>
-    )
+  return (
+    <div className="landing-page">
+      <a
+        href="https://drive.google.com/file/d/13NHhpQKkfuSmllM9EZVsaO2NVJb1uNau/view?usp=sharing"
+        target="_blank"
+        className="download-resume"
+        rel="noreferrer"
+      >
+        Download Resume
+      </a>
+      <secondaryComponents.IntroSection />
+      <secondaryComponents.MainContent />
+    </div>
+  );
 };
 export default Home;
